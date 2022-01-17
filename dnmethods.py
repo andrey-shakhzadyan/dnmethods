7f10e9f6ee78686e39e981f76a0ea7b93aec1d38
Andrey Shakhzadyan <ashakhzadyan@gmail.com>
2022-01-17 16:18:47 -0500

    fixed indenting

--- dnmethods.py
+++ dnmethods.py
@@ -2,31 +2,35 @@ import sys
 import dnfile
 
 
+def usage():
+    sys.exit(0)
+
 def main():
     path = sys.argv[1]
+    if len(sys.argv) == 2:
+        options = "tmpf"
     if len(sys.argv) == 3:
         options = sys.argv[2]
     else:
-        options = "tmpf"
+        usage()
 
-        pe = dnfile.dnPE(path)
+    pe = dnfile.dnPE(path)
 
-        if "t" in options:
-            for typerow in pe.net.mdtables.TypeDef:
-                print("Class \"" + typerow.TypeName + "\":")
-                if "m" in options:
-                    print("Methods:")
-                    for methodrow in typerow.MethodList:
-                        print("\t" + methodrow.row.Name)
-                        if "p" in options:
-                            print("\t\tParameters:")
-                            for paramrow in methodrow.row.ParamList:
-                                print("\t\t\t" + paramrow.row.Name)
-                    
-                                if "f" in options:
-                                    print("Fields:")
-                                    for fieldrow in typerow.FieldList:
-                                        print("\t" + fieldrow.row.Name)
+    if "t" in options:
+        for typerow in pe.net.mdtables.TypeDef:
+            print("Class \"" + typerow.TypeName + "\":")
+            if "m" in options:
+                print("Methods:")
+                for methodrow in typerow.MethodList:
+                    print("\t" + methodrow.row.Name)
+                    if "p" in options:
+                        print("\t\tParameters:")
+                        for paramrow in methodrow.row.ParamList:
+                            print("\t\t\t" + paramrow.row.Name)
+                            if "f" in options:
+                                print("Fields:")
+                                for fieldrow in typerow.FieldList:
+                                    print("\t" + fieldrow.row.Name)
     
 if __name__ == "__main__":
     main()
