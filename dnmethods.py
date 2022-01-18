3081948398a8b7804eae5b292103a5c9c722b036
Andrey Shakhzadyan <ashakhzadyan@gmail.com>
2022-01-17 23:40:30 -0500

    integrated argparse

--- dnmethods.py
+++ dnmethods.py
@@ -1,33 +1,32 @@
 import sys
 import dnfile
+import argparse
+
 
 
 def usage():
     sys.exit(0)
 
 def main():
-    path = sys.argv[1]
-    if len(sys.argv) == 2:
-        options = "tmpf"
-    if len(sys.argv) == 3:
-        options = sys.argv[2]
-    else:
-        usage()
+    parser = argparse.ArgumentParser(description = "Dump methods from a C# portable executable file.")
+    parser.add_argument("path", metavar="filepath", type=str, help="path of the C# PE file")
+    parser.add_argument("options", metavar="options", type=str, help="what to dump (t: class, m: methods, p: parameters, f: fields)", default="tmpf")
+    args = parser.parse_args()
+    
+    pe = dnfile.dnPE(args.path)
 
-    pe = dnfile.dnPE(path)
-
-    if "t" in options:
+    if "t" in args.options:
         for typerow in pe.net.mdtables.TypeDef:
             print("Class \"" + typerow.TypeName + "\":")
-            if "m" in options:
+            if "m" in args.options:
                 print("Methods:")
                 for methodrow in typerow.MethodList:
                     print("\t" + methodrow.row.Name)
-                    if "p" in options:
+                    if "p" in args.options:
                         print("\t\tParameters:")
                         for paramrow in methodrow.row.ParamList:
                             print("\t\t\t" + paramrow.row.Name)
-                            if "f" in options:
+                            if "f" in args.options:
                                 print("Fields:")
                                 for fieldrow in typerow.FieldList:
                                     print("\t" + fieldrow.row.Name)
