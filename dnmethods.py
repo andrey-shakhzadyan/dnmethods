61fdeda36287b3ac7562706a42cf5736e10ad69d
Andrey Shakhzadyan <ashakhzadyan@gmail.com>
2022-01-18 02:22:41 -0500

    options argument to set

--- dnmethods.py
+++ dnmethods.py
@@ -12,21 +12,21 @@ def main():
     parser.add_argument("path", metavar="filepath", type=str, help="path of the C# PE file")
     parser.add_argument("options", metavar="options", type=str, help="what to dump (t: class, m: methods, p: parameters, f: fields)", default="tmpf")
     args = parser.parse_args()
-    
+    options = set(args.options)
     pe = dnfile.dnPE(args.path)
 
-    if "t" in args.options:
+    if "t" in options:
         for typerow in pe.net.mdtables.TypeDef:
             print("Class \"" + typerow.TypeName + "\":")
-            if "m" in args.options:
+            if "m" in options:
                 print("Methods:")
                 for methodrow in typerow.MethodList:
                     print("\t" + methodrow.row.Name)
-                    if "p" in args.options:
+                    if "p" in options:
                         print("\t\tParameters:")
                         for paramrow in methodrow.row.ParamList:
                             print("\t\t\t" + paramrow.row.Name)
-                            if "f" in args.options:
+                            if "f" in options:
                                 print("Fields:")
                                 for fieldrow in typerow.FieldList:
                                     print("\t" + fieldrow.row.Name)
